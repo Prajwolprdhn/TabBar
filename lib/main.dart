4e0e2a4420da0ff0989af126091fc950d2b2d4ef
prajwolpradhan <prajwolp81@gmail.com>
2023-06-22 19:35:54 +0545

    prajwol

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,18 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+enum Fruit { apple, banana }
+
 class _HomePageState extends State<HomePage> {
+  String dropdownValue = 'Nepal';
+  var items = [
+    'Nepal',
+    'America',
+    'Australia',
+    'Japan',
+    'Canada',
+  ];
+  Fruit? _fruit = Fruit.apple;
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -43,23 +54,23 @@ class _HomePageState extends State<HomePage> {
             )
           ]),
         ),
-        body: const TabBarView(children: [
-          ListTile(
+        body: TabBarView(children: [
+          const ListTile(
             // leading: Icon(Icons.handshake),
             trailing: Icon(Icons.flutter_dash),
             title: Text('Welcome'),
             subtitle: Text('Hello everyone welcome to my channel'),
           ),
           Padding(
-            padding: EdgeInsets.all(30.0),
+            padding: const EdgeInsets.all(30.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SelectableText('Please copy this text'),
-                SizedBox(
+                const SelectableText('Please copy this text'),
+                const SizedBox(
                   height: 30,
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 200,
                   child: TextField(
                     maxLines: null,
@@ -71,13 +82,100 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  color: Colors.greenAccent,
+                  child: DropdownButton(
+                    value: dropdownValue,
+                    focusColor: Colors.red,
+                    items: items.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    // After selecting the desired option,it will
+                    // change button value to selected value
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdownValue = newValue!;
+                      });
+                    },
+                  ),
+                ),
+                RadioListTile<Fruit>(
+                  title: const Text('Apple'),
+                  value: Fruit.apple,
+                  groupValue: _fruit,
+                  onChanged: (Fruit? value) {
+                    setState(() {
+                      _fruit = value;
+                    });
+                  },
+                ),
+                RadioListTile<Fruit>(
+                  title: const Text('Banana'),
+                  value: Fruit.banana,
+                  groupValue: _fruit,
+                  onChanged: (Fruit? value) {
+                    setState(() {
+                      _fruit = value;
+                    });
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: _showAlertDialog, child: const Text('Submit')),
+                const SizedBox(
+                  height: 30,
+                ),
+                RichText(
+                  text: const TextSpan(
+                    text: 'Helllo',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Namaste',
+                          style: TextStyle(color: Colors.blue, fontSize: 16)),
+                      TextSpan(
+                          text: 'Welcome',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w500))
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-          Icon(
-            Icons.bus_alert,
-            size: 350,
-          )
+          DataTable(columns: const [
+            DataColumn(label: Text('ID')),
+            DataColumn(label: Text('First Name')),
+            DataColumn(label: Text('Last Name')),
+            DataColumn(label: Text('Age'))
+          ], rows: const [
+            DataRow(cells: [
+              DataCell(Text('1')),
+              DataCell(Text('Prajwol')),
+              DataCell(Text('Pradhan')),
+              DataCell(Text('23'))
+            ]),
+            DataRow(cells: [
+              DataCell(Text('2')),
+              DataCell(Text('Babin')),
+              DataCell(Text('Dangol')),
+              DataCell(Text('20'))
+            ])
+          ])
         ]),
         drawer: Drawer(
             child: ListView(
@@ -137,6 +235,39 @@ class _HomePageState extends State<HomePage> {
           ],
         )),
       ),
+    );
+  }
+
+  Future<void> _showAlertDialog() async {
+    return showDialog<void>(
+      context: context,
+      // barrierDismissible: false, // user must tap buttonto close the alert box
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Are you sure?'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Are you sure want to submit the text?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
